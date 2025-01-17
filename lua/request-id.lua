60a70a1bad6a7a73e9c40d69f5f6e086a6d65e38
Damon.Yang <damonops@163.com>
2025-01-17 13:46:45 +0800

    Update request-id.lua
    新增腾讯eo平台字段Eo-Log-Uuid

--- lua/request-id.lua
+++ lua/request-id.lua
@@ -8,7 +8,7 @@ local ngx = require "ngx"
 local _M = {}
 
 function _M.set_request_id()
-    local request_id_headers = {"eeo-request-id","x-nws-log-uuid","eagleid","Request-Id","X-Akamai-Request-ID"}
+    local request_id_headers = {"eeo-request-id","x-nws-log-uuid","Eo-Log-Uuid","eagleid","Request-Id","X-Akamai-Request-ID"}
     local header_request_id
     for _, header in ipairs(request_id_headers) do
         header_request_id = ngx.req.get_headers()[header]
